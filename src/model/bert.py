7376dcb50494f5657d1b87f9d55f2b1e5d1f6d45
Urszula Tworzydło <u.tworzydlo@stud.elka.pw.edu.pl>
2022-05-29 21:44:38 +0200

    softmax

--- src/model/bert.py
+++ src/model/bert.py
@@ -45,7 +45,7 @@ class DistilBertClassifier(nn.Module):
 
         self.bert = DistilBertModel.from_pretrained('distilbert-base-cased')
         self.dropout = nn.Dropout(dropout)
-        self.linear = nn.Linear(768, 15)
+        self.linear = nn.Linear(768, 6)
         self.relu = nn.ReLU()
 
 
